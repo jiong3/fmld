e4083e0d79abd6353b5de9b40bd95de01fe02593
Wilhelm Brasch <email@jiong3.com>
2025-11-19 06:36:50 +0100

    Update tests

--- rust/tests/txt2db2txt_test.rs
+++ rust/tests/txt2db2txt_test.rs
@@ -2,6 +2,7 @@ use std::fs::File;
 use std::io::BufWriter;
 use std::io::Write;
 
+use fmld::db_autofix;
 use rusqlite::Connection;
 
 use fmld::db_check;
@@ -19,9 +20,9 @@ fn test_txt_to_db_to_txt() {
 
     let tx = conn.transaction().unwrap();
 
-    db_edit::add_missing_symmetric_references(&tx).unwrap();
-    db_edit::add_missing_notes_and_tags_for_symmetric_references(&tx).unwrap();
-    let new_max_ext_note_id = db_edit::finalize_note_ids(&tx, 0).unwrap();
+    db_autofix::add_missing_symmetric_references(&tx).unwrap();
+    db_autofix::add_missing_notes_and_tags_for_symmetric_references(&tx).unwrap();
+    let new_max_ext_note_id = db_autofix::finalize_note_ids(&tx, 0).unwrap();
     tx.commit().unwrap();
 
     let round_trip_out_txt = db_check::round_trip_check(&conn);
